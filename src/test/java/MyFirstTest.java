3d189a2da8ee5f3a1e75ae5cd30104ed15510aa2
orgeron.kevin <orgeron.kevin@gmail.com>
2022-07-01 14:20:56 -0500

    small exercise for testing

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+
+
+public class MyFirstTest {
+
+    @Test
+    public void firstTest(){
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    public void secondTest(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void thirdTest(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void fourthTest(){
+        String language = "PHP";
+        language.contains("H"); // use assertTrue for this statement
+        language.contains("J"); // use assertFalse for this statement
+
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
+    }
+}
